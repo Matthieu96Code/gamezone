44a77d62caa65ac269f05f5e163c6c7845f93802
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-13 17:15:15 +0000

    Add app with get font function

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+// import { useFonts } from '@expo-google-fonts/nunito';
+import * as Font from 'expo-font';
+import Home from './screens/home';
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () => Font.loadAsync({
+    'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+  })
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [fontsLoaded, setFontsLoaded] = useState(true)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (fontsLoaded) {
+    return (
+      <Home />
+    );
+  } else {
+    <AppLoading 
+      startAsync={getFonts}
+      onFinish={()=> setFontsLoaded(true)}
+    />
+  }
+}

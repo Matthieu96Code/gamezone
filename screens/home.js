64849ef8044964c95ef570ec97150b25af26c41c
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-14 10:31:08 +0000

    Apply font

--- screens/home.js
+++ screens/home.js
@@ -3,8 +3,8 @@ import { StyleSheet, View, Text } from 'react-native';
 
 export default function Home() {
     return (
-        <View>
-            <Text style={styles.container}>Home Screen</Text>
+        <View style={styles.container}>
+            <Text style={styles.titleText}>Home Screen</Text>
         </View>
     )
 }
@@ -12,5 +12,9 @@ export default function Home() {
 const styles = StyleSheet.create({
     container: {
         padding: 24,
+    },
+    titleText: {
+        fontFamily: 'nunito Bold',
+        fontSize: 18,
     }
 })

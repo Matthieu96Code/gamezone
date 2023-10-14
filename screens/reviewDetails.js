c57f8aaf0ca4b592061c3560b1521bb2a9c8fa7d
Matthieu96Code <117777066+Matthieu96Code@users.noreply.github.com>
2023-10-14 10:43:27 +0000

    Replace the local styles by the global one

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,16 +1,11 @@
 import React from "react";
 import { StyleSheet, View, Text } from 'react-native';
+import { globalStyles } from "../styles/global";
 
 export default function ReviewDetails() {
     return (
-        <View>
-            <Text style={styles.container}>ReviewDetails Screen</Text>
+        <View style={globalStyles.container}>
+            <Text>ReviewDetails Screen</Text>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        padding: 24,
-    }
-})
